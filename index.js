ad5c080ed5874b8667823a0bbe3c078a203ab622
Tadhg McIntyre <mcintyretadhg@gmail.com>
2021-03-31 13:05:15 -0400

    added prompts for inquirer to use depending on what kind of employee is being made

--- index.js
+++ index.js
@@ -4,3 +4,68 @@ const Manager = require('./class-lib/Manager');
 const Engineer = require('./class-lib/Engineer');
 const Intern = require('./class-lib/Intern');
 const fs = require('fs');
+const { async } = require('rxjs');
+
+function runInquirer(){
+    const initPrompt = [{
+        type: 'confirm',
+        message: 'What is your job title?',
+        name: 'title',
+        choices: ['Manager', 'Engineer', 'Intern'],
+    }, ];
+    return inquirer
+        .prompt(initPrompt);
+};
+
+const runInquirerEmployee = () => {
+    const employeePrompts = [{
+        type: 'input',
+        message: 'What is this persons name?',
+        name: 'name',
+    }, {
+        type: 'input',
+        message: 'What is this persons ID?',
+        name: 'id',
+    }, {
+        type: 'input',
+        message: 'What is this persons email?',
+        name: 'email',
+    }, ];
+    return inquirer
+        .prompt(employeePrompts);
+};
+
+const runInquirerManager = () => {
+    const managerPrompt = [{
+        type: 'input',
+        message: `What is this manager's office number?`,
+        name: 'officeNumber', 
+    }, ];
+    return inquirer
+        .prompt(managerPrompt)
+};
+
+const runInquirerEngineer = () => {
+    const engineerPrompt = [{
+        type: 'input',
+        message: `What is this Engineer's GitHub account name?`,
+        name: 'github',
+    },];
+    return inquirer
+        .prompt(engineerPrompt);
+};
+
+const runInquirerIntern = () => {
+    const internPrompt = [{
+        type: 'input',
+        message: 'What school does this Intern attend?',
+        name: 'school',
+    }, ];
+    return inquirer
+        .prompt(internPrompt);
+};
+
+async function buildTeam(){
+    let employeeArray = [];
+
+}
